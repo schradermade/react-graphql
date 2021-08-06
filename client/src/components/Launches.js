5a9f90a049204d98df17800c74295e1353d6ad0c
Nathan Schrader <natecschrader@gmail.com>
2021-08-06 13:23:58 -0700

    added Client

---
+++ client/src/components/Launches.js
@@ -0,0 +1,50 @@
+import React, { Component, Fragment } from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import "./Launches.css";
+import LaunchItem from "./LaunchItem";
+
+//Query created with gql
+const LAUNCHES_QUERY = gql`
+  query LaunchesQuery {
+    launches {
+      flight_number
+      mission_name
+      launch_date_local
+      launch_success
+    }
+  }
+`;
+
+export class Launches extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div>
+          <h1 className="display-4 my-3">Launches</h1>
+          <Query query={LAUNCHES_QUERY}>
+            {({ loading, error, data }) => {
+              if (loading)
+                return (
+                  <div class="lds-ripple">
+                    <div></div>
+                    <div></div>
+                  </div>
+                );
+              if (error) console.log(error);
+
+              return <Fragment>
+                {
+                  data.launches.map(launch => (
+                    <LaunchItem key={launch.flight_number} launch={launch}/>
+                  ))}
+              </Fragment>;
+            }}
+          </Query>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default Launches;
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const cors = require('cors');
+const schema = require("./schema");
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true
-}));
+// Allow cross-origin
+app.use(cors());
 
-const PORT = process.env.PORT || 5000
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
